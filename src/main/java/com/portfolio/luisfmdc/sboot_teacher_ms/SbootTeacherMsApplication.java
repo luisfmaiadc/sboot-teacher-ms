1a01c5ab975db403e5ebebe6603faa04a1427e76
luisfmaiadc <lf.mcosta23@gmail.com>
2025-02-12 16:18:00 -0300

    Inicialização do versionamento do projeto.

---
+++ src/main/java/com/portfolio/luisfmdc/sboot_teacher_ms/SbootTeacherMsApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.luisfmdc.sboot_teacher_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbootTeacherMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbootTeacherMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfolio/luisfmdc/sboot_teacher_ms/SbootTeacherMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.luisfmdc.sboot_teacher_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbootTeacherMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
